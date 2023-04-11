3d3e9300b15cc557fe99fdfb1fa5a5fd11b8d3ab
Maksim <maksim.legeyda@yandex.ru>
2023-04-11 18:47:52 +0300

    'update'

--- Discount_Monitoring/settings.py
+++ Discount_Monitoring/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Discount_Monitoring.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
