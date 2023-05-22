e2051bab3e43be232a2ff2b6e567836453b160c0
Maksim Legeyda <maksim.legeyda@yandex.ru>
2023-05-22 10:47:15 +0300

    add telebot

--- discount_bot/bot.py
+++ discount_bot/bot.py
@@ -1,18 +1,37 @@
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from shops.models import Shop
+import telebot
+from datetime import datetime, timedelta
 from Discount_Monitoring.settings import BOT_TOKEN
+from discount.models import DiscountData
+from django.db.models import Count
 
 
-logging.basicConfig(level=logging.INFO)
+bot=telebot.TeleBot(BOT_TOKEN)
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+@bot.message_handler(commands=['discount'])
+def start_message(message):
+    now = datetime.now()
+    discountCurrent = DiscountData.objects.filter(startDate__lte=now).filter(endDate__gte=now).order_by('-startDate')
+    bot.send_message(message.chat.id, f"Привет ✌️/n Список акций: {discountCurrent}")
 
-
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome_message(message: types.Message):
-    await message.reply('Привет!')
+@bot.message_handler(commands=['count'])
+def start_message(message):
+    now = datetime.now()
+    allCurrentDiscountCounter = DiscountData.objects.filter(startDate__lte=now).filter(endDate__gte=now).count()
+    bot.send_message(message.chat.id, f"Количество действующих акций:  {allCurrentDiscountCounter}")
     
+@bot.message_handler(commands=['top'])
+def start_message(message):
+    allShops = Shop.objects.all()
+    topByNumberOfDiscount = allShops.annotate(num_discount=Count('shop')).order_by('-countCurrentDiscount')
+    bot.send_message(message.chat.id, f"Топ сетей по количеству действующих акций:  {topByNumberOfDiscount}")
     
+@bot.message_handler(commands=['download'])
+def start_message(message):
+    allShops = Shop.objects.all()
+    topByNumberOfDiscount = allShops.annotate(num_discount=Count('shop')).order_by('-countCurrentDiscount')
+    bot.send_message(message.chat.id, f"Топ сетей по количеству действующих акций:  {topByNumberOfDiscount}")
+  
+bot.infinity_polling()
 
-executor.start_polling(dp, skip_updates=True)
--- discount_bot/management/commands/runbot.py
+++ discount_bot/management/commands/runbot.py
@@ -1,4 +1,13 @@
+from typing import Any, Optional
 from django.core.management.base import BaseCommand, CommandError
+from Discount_Monitoring.settings import BOT_TOKEN
+from discount_bot import bot
+import telebot
 
 class Command(BaseCommand):
     help = 'Start telegram bot'
+    
+    def handle(self, *args: Any, **options: Any) -> str | None:
+        print('Запуска бота...')
+        bot.start_message()
+        
